5840c5044c5620a0c2701c32636516ea434ddc63
Schobben Thomas <schobben.thomas@gmail.com>
2016-05-22 22:51:18 +0200

    Had to adapt the test too.
    Works on linux & OSX too.

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,29 @@
+'use strict';
+
 var gulp = require('gulp');
 var es3ify = require("gulp-es3ify");
-var derequire = require("derequire");
 var uglify = require('gulp-uglify');
 var rename = require("gulp-rename");
+var jshint = require('gulp-jshint');
 
-var _pluginname = "yourpluginname";
+var pluginname = 'mypluginname';
+var jsHintReport = 'default';
+var jsHintPaths = {
+  js : ['./*.js', './test/test.js']
+};
 
 gulp.task('afterbrowserify', function() {
   gulp.src('./temp-browserify/index.js')
   .pipe(es3ify())
-  .pipe(rename("pouchdb."+_pluginname+".js"))
+  .pipe(rename('pouchdb.'+pluginname+'.js'))
   .pipe(gulp.dest('./dist/'))
   .pipe(uglify())
-  .pipe(rename("pouchdb."+_pluginname+".min.js"))
-  .pipe(gulp.dest('./dist/'))
+  .pipe(rename('pouchdb.'+pluginname+'.min.js'))
+  .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('jshint', function() {
+  return gulp.src(jsHintPaths.js)
+    .pipe(jshint())
+    .pipe(jshint.reporter(jsHintReport));
 });
